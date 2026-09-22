730436416ba9fc69da95b1b08e2a16427eadcf1c
ScriptedBy <damarcusj014@gmail.com>
2026-09-22 17:57:19 -0500

    AnimationsModule.lua

---
+++ AnimationsModule.lua
@@ -0,0 +1,95 @@
+--// Stores animation IDs for different characters that are equipped. (unfinished)
+
+local _Animations = {
+
+	["Security Toilet"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://14045736429"
+	},
+	["Security Helper"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://14045736429"
+	},
+	["Security Medic"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://507766388"
+	},
+	["VIP Security"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://507766388"
+	},
+	["Handsaw Security"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://507766388"
+	},
+	["Cameraman"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://507766388"
+	},
+	["Speakerman"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://507766388"
+	},
+	["OG Speakerman"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://507766388"
+	},
+	["Upgraded Cameraman"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://507766388"
+	},
+	
+	["Upgraded Speakerman"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://507766388"
+	},
+	
+	["TV Man"] = {
+		Attack = "",
+		Death = "",
+		Idle = "rbxassetid://507766388"
+	},
+	["Titan Speakerman"] = {
+		Attack = "",
+		Death = " ",
+		Idle = "rbxassetid://14045734197"
+	},
+	["Titan Cameraman"] = {
+		Attack = "",
+		Death = " ",
+		Idle = "rbxassetid://14045734197"
+	},
+
+	["Skibi"] = {
+		Attack = "rbxassetid://14045731748",
+		Death = "",
+		Idle = "rbxassetid://14045735449"
+	},
+	
+	["Girl Skibi"] = {
+		Attack = "rbxassetid://14045731748",
+		Death = "",
+		Idle = "rbxassetid://14045735449"
+	},
+	
+	["Giant Skibi"] = {
+		Attack = "rbxassetid://14045731748",
+		Death = "",
+		Idle = "rbxassetid://14045735449"
+	}
+
+}
+
+
+return _Animations; 
+
