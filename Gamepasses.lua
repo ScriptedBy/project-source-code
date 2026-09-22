5e4546f0dad6a443f8de00dffa4bc22b54b0645e
ScriptedBy <damarcusj014@gmail.com>
2026-09-22 18:27:01 -0500

    Create Gamepasses.lua

---
+++ Gamepasses.lua
@@ -0,0 +1,32 @@
+--// Organizes game pass and developer product IDs for premium upgrades and in-game currency purchases.
+
+local __Gamepasses = {
+	
+	Passes = {
+		["5 Equip"] = 200777907,
+		["Double Corrupted Luck"] = 199492731,
+		["Double Crate Luck"] = 199492272,
+		["Double Defender XP"] = 199495369, 
+		["Double Tokens"] = 199492932, 
+		["Triple Open"] = 199493120,
+		["VIP"] = 199501207,
+	},
+	
+	Products = {
+		-- Developer Products
+		["1000 Coins"] = 1572712904,
+		["10000 Coins"] = 1572712994,
+		["50000 Coins"] = 1572713198,
+		["100000 Coins"] = 1572713376, 
+		["500000 Coins"] = 1572713627, 
+		
+		["200 Diamonds"] = 1572713770,
+		["1000 Diamonds"] = 1572713857,
+		["5000 Diamonds"] = 1572713928,
+		["10000 Diamonds"] = 1572714116, 
+		["50000 Diamonds"] = 1572714258, 
+	}
+}
+
+
+return __Gamepasses;
